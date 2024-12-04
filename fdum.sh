497c6fd1be74c44d5308721558eab0a7cb0fce41
blindseer1 <vladtiru05@gmail.com>
2024-12-04 15:22:04 +0200

    Add fdum_init

--- fdum.sh
+++ fdum.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-if find ./.fdum/ -mindepth 1 -maxdepth 1 | read; then
-  echo "dir not empty"
-else
-  echo "dir empty"
+fdum_init() {
+  if [ ! -d ".fdum" ]; then
+    mkdir .fdum
+    echo "Directory .fdum created."
+  else
+    echo "Directory .fdum already exists."
+    if find ./.fdum/ -mindepth 1 -maxdepth 1 | read; then
+      echo "dir not empty"
+    else
+      echo "dir empty"
+    fi
+  fi
+}
+
+if [ "$1" == "init" ]; then
+  fdum_init
 fi
