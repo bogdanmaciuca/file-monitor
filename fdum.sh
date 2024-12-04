c4893fb5b8b745367edd82eeca89cfd442681bdb
Bogdan-Alexandru Maciuca <103450581+bogdanmaciuca@users.noreply.github.com>
2024-12-04 15:31:00 +0200

    fixed bugs

--- fdum.sh
+++ fdum.sh
@@ -6,15 +6,24 @@ fdum_init() {
     echo "Directory .fdum created."
   else
     echo "Directory .fdum already exists."
-    if find ./.fdum/ -mindepth 1 -maxdepth 1 | read; then
-      echo "dir not empty"
-    else
-      echo "dir empty"
-    fi
+    #if find ./.fdum/ -mindepth 1 -maxdepth 1 | read; then
+    #  echo "dir not empty"
+    #else
+    #  echo "dir empty"
+    #fi
   fi
 }
 
 if [ $# = 1 ]; then
+    if [ "$1" = "init" ]; then
+        fdum_init
+    elif [ "$1" = "diff" ]; then
+        echo "diff take either 1 or 2 arguments"
+    elif [ "$1" = "snapshot" ]; then
+        fdum_snap
+    else
+        echo "Unknown argument: $1"
+    fi
 elif [ $# = 2 ]; then
     echo "2 arguments!"
 elif [ $# = 3 ]; then
